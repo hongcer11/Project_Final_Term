f569adae615e7d90d4d7257b4f9e7733a4339284
Đinh Hồng Cơ <119274173+hongcer11@users.noreply.github.com>
2023-04-02 18:13:39 +0700

    Update main.py

--- main.py
+++ main.py
@@ -1,9 +1,2 @@
 print("đây là file giao diện")
-<<<<<<< HEAD
-print("đây là file giao diện")
-print("đây là file giao diện")
 
-print(" chào Hồng Cơ")
-=======
-print("")
->>>>>>> parent of 2d32972 (Revert "Update main.py")
